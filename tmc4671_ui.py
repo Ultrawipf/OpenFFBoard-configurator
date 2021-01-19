ab77e845db41f90c144d7d310a0c745f5f0f0815
Yannick Richter <git@y-richter.de>
2021-01-19 21:16:41 +0100

    fixed abs in tmc

--- tmc4671_ui.py
+++ tmc4671_ui.py
@@ -52,7 +52,7 @@ class TMC4671Ui(WidgetUI):
     def updateCurrent(self,current):
    
         try:
-            current = float(abs(current))
+            current = abs(float(current))
             v = (2.5/0x7fff) * current
             amps = round((v / self.amp_gain) / self.shunt_ohm,3)
             self.label_Current.setText(str(amps)+"A")
