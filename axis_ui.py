379899ef6c9f04dd0638d6564774da0f57ed45d9
Yannick Richter <git@y-richter.de>
2021-03-16 15:55:54 +0100

    Moved encoder settings to widget

--- axis_ui.py
+++ axis_ui.py
@@ -9,6 +9,7 @@ import main
 import buttonconf_ui
 import analogconf_ui
 from base_ui import WidgetUI
+from encoderconf_ui import EncoderOptions
 
 class AxisUI(WidgetUI):
 
@@ -25,6 +26,7 @@ class AxisUI(WidgetUI):
     encId = 0
 
     axis = 'X'
+    encWidgets = {}
 
     def __init__(self, main=None, unique='X'):
         WidgetUI.__init__(self, main, 'axis_ui.ui')
@@ -41,6 +43,7 @@ class AxisUI(WidgetUI):
         self.horizontalSlider_fxratio.valueChanged.connect(self.fxratio_changed)
         self.horizontalSlider_cffilter.valueChanged.connect(self.cffilter_changed)
 
+        #self.comboBox_encoder.currentIndexChanged.connect(self.encoderIndexChanged)
 
         self.checkBox_invert.stateChanged.connect(lambda val : self.serialWrite("invert="+("0" if val == 0 else "1")+"\n"))
 
@@ -85,8 +88,7 @@ class AxisUI(WidgetUI):
 
     def power_changed(self,val):
         self.serialWrite("power="+str(val)+"\n")
-        text = str(val)     
-
+        text = str(val)
         # If tmc is used show a current estimate
         if(self.drvId == 1):
             v = (2.5/0x7fff) * val
@@ -105,10 +107,10 @@ class AxisUI(WidgetUI):
 
 
     def submitHw(self):
-        val = self.spinBox_cpr.value()
+        #val = self.spinBox_cpr.value()
         self.driverChanged(self.comboBox_driver.currentIndex())
         self.encoderChanged(self.comboBox_encoder.currentIndex())
-        self.serialWrite("cpr="+str(val)+"\n")
+        #self.serialWrite("cpr="+str(val)+"\n")
 
 
     def driverChanged(self,idx):
@@ -132,6 +134,7 @@ class AxisUI(WidgetUI):
             self.getEncoder()
             self.main.updateTabs()
             self.updateSliders()
+            self.encoderIndexChanged(id)
         
     
     def updateSliders(self):
@@ -141,7 +144,6 @@ class AxisUI(WidgetUI):
             self.horizontalSlider_power.setMaximum(28000)
         else:
             self.horizontalSlider_power.setMaximum(0x7fff)
-
         callbacks = [
         self.horizontalSlider_power.setValue,
         self.horizontalSlider_degrees.setValue,
@@ -170,19 +172,30 @@ class AxisUI(WidgetUI):
                 self.comboBox_driver.setCurrentIndex(self.drvIds[self.drvId][0])
         self.serialGetAsync(["drvtype!","drvtype?"],drvtypecb)
        
+    def encoderIndexChanged(self,idx):
+        id = self.comboBox_encoder.currentData()
+        if(id not in self.encWidgets):
+            return
+        self.stackedWidget_encoder.setCurrentWidget(self.encWidgets[id])
 
     def getEncoder(self):
         #self.comboBox_encoder.currentIndexChanged.disconnect()
-        self.spinBox_cpr.setEnabled(True)
+        #self.spinBox_cpr.setEnabled(True)
 
         
         def f(dat):
             self.comboBox_encoder.clear()
+            self.encWidgets.clear()
+
             self.encIds,self.encClasses = classlistToIds(dat)
             for c in self.encClasses:
-                self.comboBox_encoder.addItem(c[1])
-        self.serialGetAsync("enctype!",f)
+                self.comboBox_encoder.addItem(c[1],c[0])
+                id = c[0]
+                if(id not in self.encWidgets or self.stackedWidget_encoder.indexOf(self.encWidgets[id]) == -1):
+                    self.encWidgets[id] = EncoderOptions(self.main,id)
+                    self.stackedWidget_encoder.addWidget(self.encWidgets[id])
 
+        self.serialGetAsync("enctype!",f)
         
         def encid_f(id):
             if(id == None):
@@ -192,14 +205,14 @@ class AxisUI(WidgetUI):
             
             idx = self.encIds[self.encId][0] if self.encId in self.encIds else 0
             self.comboBox_encoder.setCurrentIndex(idx)
-                        
-
-            if(self.encId == 1):
-                self.spinBox_cpr.setEnabled(False)
+            self.encoderIndexChanged(idx)
+            
+            # if(self.encId == 1):
+            #     self.spinBox_cpr.setEnabled(False)
         self.serialGetAsync("enctype?",encid_f,int)
-        def f_cpr(v):
-            self.spinBox_cpr.setValue(v)
-        self.serialGetAsync("cpr?",f_cpr,int)
+        # def f_cpr(v):
+        #     self.spinBox_cpr.setValue(v)
+        # self.serialGetAsync("cpr?",f_cpr,int)
 
 
     # Prepend the axis letter to the command before sending   
