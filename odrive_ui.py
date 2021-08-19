e5fd3c6e42a20f85d4ae812cfb925287329c2297
Yannick Richter <git@y-richter.de>
2021-08-19 18:24:12 +0200

    Odrive errors as text

--- odrive_ui.py
+++ odrive_ui.py
@@ -11,6 +11,7 @@ from base_ui import WidgetUI
 class OdriveUI(WidgetUI):
     prefix = None
     odriveStates = ["AXIS_STATE_UNDEFINED","AXIS_STATE_IDLE","AXIS_STATE_STARTUP_SEQUENCE","AXIS_STATE_FULL_CALIBRATION_SEQUENCE","AXIS_STATE_MOTOR_CALIBRATION","-","AXIS_STATE_ENCODER_INDEX_SEARCH","AXIS_STATE_ENCODER_OFFSET_CALIBRATION","AXIS_STATE_CLOSED_LOOP_CONTROL","AXIS_STATE_LOCKIN_SPIN","AXIS_STATE_ENCODER_DIR_FIND","AXIS_STATE_HOMING","AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION","AXIS_STATE_ENCODER_HALL_PHASE_CALIBRATION"]
+    odriveErrors = ["ODRIVE_ERROR_CONTROL_ITERATION_MISSED","ODRIVE_ERROR_DC_BUS_UNDER_VOLTAGE","ODRIVE_ERROR_DC_BUS_OVER_VOLTAGE","ODRIVE_ERROR_DC_BUS_OVER_REGEN_CURRENT","ODRIVE_ERROR_DC_BUS_OVER_CURRENT","ODRIVE_ERROR_BRAKE_DEADTIME_VIOLATION","ODRIVE_ERROR_BRAKE_DUTY_CYCLE_NAN","ODRIVE_ERROR_INVALID_BRAKE_RESISTANCE"]
     def __init__(self, main=None, unique=None):
         WidgetUI.__init__(self, main,'odrive.ui')
         self.main = main #type: main.MainUi
@@ -54,10 +55,22 @@ class OdriveUI(WidgetUI):
     def updateTorque(self,torque):
         self.doubleSpinBox_torque.setValue(torque/100)
 
+    def shorErrors(self,codes):
+        errString = ""
+        if(codes == 0):
+            errString = "None"
+        for i,name in enumerate(self.odriveErrors):
+            if(codes & 1 << i) != 0:
+                errString += name + "\n"
+
+        self.label_errornames.setText(errString)
+
     def statusUpdateCb(self,dat):
         self.label_voltage.setText("{}V".format(dat[0]/1000))
-        self.label_errors.setText("{:02x}".format(dat[1]))
+        #self.label_errors.setText("{:02x}".format(dat[1]))
+        self.shorErrors(dat[1])
         self.label_state.setText(self.odriveStates[dat[2]])
+        
 
 
     def updateTimer(self):
