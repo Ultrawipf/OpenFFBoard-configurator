0758512072fd1c4aa0c5ed0a9da8231538d6805e
Yannick Richter <git@y-richter.de>
2022-04-08 11:49:56 +0200

    Added analog can and can frame preview

--- analogconf_ui.py
+++ analogconf_ui.py
@@ -13,8 +13,10 @@ class AnalogOptionsDialog(OptionsDialog):
         self.main = main
         self.dialog = OptionsDialogGroupBox(name,main)
 
-        if(id == 0): # local buttons
+        if(id == 0): # local analog
             self.dialog = (AnalogInputConf(name,self.main))
+        if(id == 1): # can analog
+            self.dialog = (CANAnalogConf(name,self.main))
 
         OptionsDialog.__init__(self, self.dialog,main)
 
@@ -75,3 +77,69 @@ class AnalogInputConf(OptionsDialogGroupBox,CommunicationHandler):
 
     def onclose(self):
         self.removeCallbacks()
+
+
+class CANAnalogConf(OptionsDialogGroupBox,CommunicationHandler):
+
+    def __init__(self,name,main):
+        self.main = main
+        OptionsDialogGroupBox.__init__(self,name,main)
+        CommunicationHandler.__init__(self)
+
+
+    def initUI(self):
+        vbox = QVBoxLayout()
+
+        self.numAinBox = QSpinBox()
+        self.numAinBox.setMinimum(1)
+        self.numAinBox.setMaximum(8)
+        vbox.addWidget(QLabel("Number of axes"))
+        vbox.addWidget(self.numAinBox)
+        self.numAinBox.valueChanged.connect(self.amountChanged)
+
+        self.canIdBox = QSpinBox()
+        self.canIdBox.setMinimum(1)
+        self.canIdBox.setMaximum(0x7ff)
+        vbox.addWidget(QLabel("CAN frame ID"))
+        vbox.addWidget(self.canIdBox)
+        self.canIdBox.valueChanged.connect(self.amountChanged)
+
+        self.infoLabel = QLabel("")
+        vbox.addWidget(self.infoLabel)
+
+        vbox.addWidget(QLabel("CAN Speed default 500k"))
+        
+        self.setLayout(vbox)
+
+    def onclose(self):
+        self.removeCallbacks()
+
+    def amountChanged(self,_):
+        amount = self.numAinBox.value()
+        text = ""
+        for packet in range(int((amount-1)/4)+1):
+            text += f"ID {self.canIdBox.value()+packet}:\n("
+            for value in range(4):
+                if(value < int(amount - int(packet*4))):
+                    text += f"| v{value+1+packet*4}[0:7], v{value+1+packet*4}[8:15] |"
+                else:
+                    text += "|xx||xx|"
+            text += ")\n"
+
+        self.infoLabel.setText(text)
+
+    def apply(self):
+        self.sendValue("cananalog","canid",self.canIdBox.value())
+        self.sendValue("cananalog","amount",self.numAinBox.value())
+
+    def maximumCb(self,val):
+        self.numAinBox.setMaximum(val)
+        self.canIdBox.setMaximum(0x7ff - int((val-1)/4))
+    
+    def readValues(self):
+        self.getValueAsync("cananalog","amount",self.numAinBox.setValue,0,conversion=int)
+        self.getValueAsync("cananalog","maxamount",self.maximumCb,0,conversion=int)
+        self.getValueAsync("cananalog","canid",self.canIdBox.setValue,0,conversion=int)
+
+        
+ 
