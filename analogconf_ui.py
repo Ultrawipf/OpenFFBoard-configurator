e6b2ee3f3cb7b85ef569ef5d8afeb9706202ab08
Yannick Richter <git@y-richter.de>
2022-07-26 17:43:10 +0200

    Added rangeslider for analog conf

--- analogconf_ui.py
+++ analogconf_ui.py
@@ -1,7 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow
-from PyQt6.QtWidgets import QDialog
-from PyQt6.QtWidgets import QWidget,QGroupBox,QProgressBar
-from PyQt6.QtWidgets import QMessageBox,QVBoxLayout,QCheckBox,QButtonGroup,QPushButton,QLabel,QSpinBox,QComboBox,QFormLayout
+from PyQt6.QtWidgets import QDialog,QSizePolicy
+from PyQt6.QtWidgets import QWidget,QGroupBox,QProgressBar,QSpacerItem 
+from PyQt6.QtWidgets import QMessageBox,QVBoxLayout,QHBoxLayout,QGridLayout,QCheckBox,QButtonGroup,QPushButton,QLabel,QSpinBox,QComboBox,QFormLayout
 from PyQt6 import uic
 from PyQt6.QtCore import QTimer
 import main
@@ -9,22 +9,40 @@ from helper import res_path,classlistToIds,updateListComboBox
 from optionsdialog import OptionsDialog,OptionsDialogGroupBox
 from base_ui import CommunicationHandler
 import portconf_ui
+from qrangeslider import QtRangeSlider
 
 
 # Helper widget implementing common commands for analog axes
 class AnalogProcessingOptions(QWidget,CommunicationHandler):
-    def __init__(self, parent,classname : str, instance : int = 0, autoscale : bool = False, filter : bool = False, manual_tune_channels : int = 0):
+    def __init__(self, parent,classname : str, instance : int = 0, autoscale : bool = False, filter : bool = False,values : bool = True, manual_tune : bool = False, channels : int = 0):
         QWidget.__init__(self,parent=parent)
         CommunicationHandler.__init__(self)
         self.layout = QVBoxLayout()
         self.layout.setContentsMargins(0,0,0,0)
         self.setLayout(self.layout)
 
-        
+        self.channels = channels
         self.classname = classname
         self.instance = instance
         self.filter = filter
         self.autoscale = autoscale
+        self.manual_tune = manual_tune
+        self.values = values
+        self.timer = QTimer(self)
+        self.pgb_list = []
+
+        if self.manual_tune:
+            self.tune_list = []
+            self.tuneBox = QGroupBox("Manual limits")
+            self.tuneBoxLayout = QGridLayout()
+            self.tuneBox.setLayout(self.tuneBoxLayout)
+            self.layout.addWidget(self.tuneBox)
+
+        if self.values:
+            self.pgbBox = QGroupBox("Output values")
+            self.pgbBoxLayout = QVBoxLayout()
+            self.pgbBox.setLayout(self.pgbBoxLayout)
+            self.layout.addWidget(self.pgbBox)
 
         if filter:
             self.filterBox = QCheckBox("Lowpass filter")
@@ -34,11 +52,118 @@ class AnalogProcessingOptions(QWidget,CommunicationHandler):
         if autoscale:
             self.autorangeBox = QCheckBox("Autorange")
             self.layout.addWidget(self.autorangeBox)
+            if self.manual_tune:
+                self.autorangeBox.stateChanged.connect(lambda v: self.tuneBox.setEnabled(not v))
            # self.get_value_async("apin","autocal",self.autorangeBox.setChecked,0,conversion=int)
 
-        if manual_tune_channels > 0:
-            pass
-            # create min/max boxes or sliders
+        if channels > 0:
+            self.setChannels(channels)
+           
+        self.readValues()
+        self.register_callback(self.classname,"values",self.valueCb,self.instance,str)
+        self.register_callback(self.classname,"rawval",self.rawValueCb,self.instance,str)
+        self.timer.timeout.connect(self.tim)
+        
+
+    def showEvent(self,event):
+        self.timer.start(300)
+
+
+    def hideEvent(self,event):
+        self.timer.stop()
+
+    def tim(self): # Timer timeout
+        if self.values:
+            self.send_command(self.classname,"values",self.instance)
+        if self.manual_tune:
+            self.send_command(self.classname,"rawval",self.instance)
+
+        if self.manual_tune and self.autorangeBox.isChecked():
+            for i in range(self.channels):
+                self.send_commands(self.classname,["min","max"],instance = self.instance,adr=i)
+
+
+    def valueCb(self, str):
+        val_list = str.split("\n")
+        j=0
+        for i in range(len(self.pgb_list)):
+            pgb = self.pgb_list[i]
+            if j < len(val_list):
+                pgb.setValue(int(val_list[j]))
+                pgb.setVisible(True)
+                j=j+1
+            else:
+                pgb.setValue(-32768)
+                pgb.setVisible(False)
+
+    def rawValueCb(self, str):
+        val_list = str.split("\n")
+        j=0
+        for i in range(len(self.tune_list)):
+            pgb = self.tune_list[i][1]
+            if j < len(val_list):
+                pgb.setValue(int(val_list[j]))
+                j=j+1
+            else:
+                pgb.setValue(0)
+        
+     # create min/max boxes or sliders
+    def setChannels(self,channels : int):
+        self.channels = channels
+        
+        if self.manual_tune:
+            for ch,row in enumerate(self.tune_list):
+                for widget in row:
+                    widget.setVisible(ch < channels)
+      
+            for i in range(channels):
+                if len(self.tune_list) >= channels:
+                    continue # Do not add more
+                
+                # minBox,maxBox = QSpinBox(self),QSpinBox(self)
+                # minBox.setRange(-0x7fff,0x7fff)
+                # maxBox.setRange(-0x7fff,0x7fff)
+                # newWidgets = [QLabel(f"Min {i+1}"),minBox,QLabel(f"Max {i+1}"),maxBox]
+                rangeSlider = QtRangeSlider(self,0xffff) # TODO fix slider to allow non 0 start values
+                
+                rawProgressBar = QProgressBar(self)
+                rawProgressBar.setRange(-32768, 32767)
+                rawProgressBar.setFixedHeight(QtRangeSlider.HEIGHT//2)
+                rawProgressBar.setTextVisible(False)
+                #rawProgressBar.setStyleSheet(f"padding :{QtRangeSlider.TRACK_PADDING}px")
+                #rawProgressBar.setFormat(f"%p% (%v) Ch{i+1}")
+                
+                
+                self.tune_list.append([rangeSlider,rawProgressBar])
+                # self.tuneBoxLayout.addWidget(rangeSlider)
+                # self.tuneBoxLayout.addWidget(rawProgressBar)
+                
+                self.tuneBoxLayout.addItem(QSpacerItem(QtRangeSlider.TRACK_PADDING,0,QSizePolicy.Policy.Fixed ) ,i,0)
+                self.tuneBoxLayout.addWidget(rawProgressBar,i,1)
+                self.tuneBoxLayout.addItem(QSpacerItem(QtRangeSlider.TRACK_PADDING,0,QSizePolicy.Policy.Fixed) ,i,2)
+                self.tuneBoxLayout.addWidget(rangeSlider,i,0,1,2)
+                #rangeSlider.setValue(0x7fff)
+                print(i)
+                self.register_callback(self.classname,"min",lambda v,slider=rangeSlider : slider.set_left_thumb_value(v+0x7fff) ,self.instance,adr=i,conversion=int)
+                self.register_callback(self.classname,"max",lambda v,slider=rangeSlider : slider.set_right_thumb_value(v+0x7fff),self.instance,adr=i,conversion=int)
+                
+                # for col,widget in enumerate(newWidgets):
+                #     self.tuneBoxLayout.addWidget(widget,i,col)
+                #     widget.setVisible(True)
+
+        if self.values:
+            for i in range(channels):
+                if len(self.pgb_list) >= channels:
+                    continue # Do not add more
+
+                pgb = QProgressBar(self)
+                pgb.setVisible(False)
+                pgb.setFixedHeight(10)
+                pgb.setRange(-32768, 32767)
+                pgb.setValue(-32768)
+                pgb.setFormat(f"%p% (%v) Ch{i+1}")
+                self.pgbBoxLayout.addWidget(pgb)
+                self.pgb_list.append(pgb)
 
         self.readValues()
 
@@ -47,6 +172,9 @@ class AnalogProcessingOptions(QWidget,CommunicationHandler):
             self.get_value_async(self.classname,"autocal",self.autorangeBox.setChecked,self.instance,conversion=int)
         if self.filter:
             self.get_value_async(self.classname,"filter",self.filterBox.setChecked,self.instance,conversion=int)
+        # if self.manual_tune:
+        #     for i in range(self.channels):
+        #         self.send_commands(self.classname,["min","max"],instance = self.instance,adr=i)
 
     def apply(self):
         if self.autoscale:
@@ -54,10 +182,15 @@ class AnalogProcessingOptions(QWidget,CommunicationHandler):
 
         if self.filter:
             self.send_value(self.classname,"filter",1 if self.filterBox.isChecked() else 0,instance = self.instance)
+        if self.manual_tune:
+            for i in range(self.channels):
+                min = self.tune_list[i][0].get_left_thumb_value() - 0x7fff
+                max = self.tune_list[i][0].get_right_thumb_value() - 0x7fff
+                self.send_value(self.classname,"min",min,instance = self.instance,adr=i)
+                self.send_value(self.classname,"max",max,instance = self.instance,adr=i)
+            self.readValues()
 
         
-
-
 
 class AnalogOptionsDialog(OptionsDialog):
     def __init__(self,name,id, main):
@@ -96,7 +229,7 @@ class AnalogInputConf(OptionsDialogGroupBox,CommunicationHandler):
 
     def initUI(self):
         layout = QVBoxLayout()
-        self.processingOptions = AnalogProcessingOptions(self,"apin",0,True,True,0)
+        self.processingOptions = AnalogProcessingOptions(self,"apin",0,filter=True,autoscale=True,values=False,manual_tune=True)
         layout.addWidget(self.processingOptions)
         # self.autorangeBox = QCheckBox("Autorange")
         # layout.addWidget(self.autorangeBox)
@@ -117,8 +250,9 @@ class AnalogInputConf(OptionsDialogGroupBox,CommunicationHandler):
         self.send_commands("apin",["values"],self.prefix)
 
     def readValues(self):
-        self.processingOptions.readValues()
+        
         self.get_value_async("apin","pins",self.createAinButtons,0,conversion=int)
+        self.processingOptions.readValues()
         # self.get_value_async("apin","autocal",self.autorangeBox.setChecked,0,conversion=int)
         # self.get_value_async("apin","filter",self.filterBox.setChecked,0,conversion=int)
 
@@ -151,6 +285,7 @@ class AnalogInputConf(OptionsDialogGroupBox,CommunicationHandler):
             for i in range(self.axes):
                 self.analogbtns.button(i).setChecked(axismask & (1 << i))
         self.get_value_async("apin","mask",f,0,conversion=int)
+        self.processingOptions.setChannels(int(axes))
 
     def valueCb(self, str):
         val_list = str.split("\n")
@@ -259,7 +394,7 @@ class ADS111XAnalogConf(OptionsDialogGroupBox,CommunicationHandler):
     def initUI(self):
         layout = QFormLayout()
 
-        self.processingOptions = AnalogProcessingOptions(self,"adsAnalog",0,True,True,0)
+        self.processingOptions = AnalogProcessingOptions(self,"adsAnalog",0,filter=True,autoscale=True,values=False,manual_tune=False,channels = 4)
         layout.addRow(self.processingOptions)
 
         self.numAinBox = QSpinBox()
@@ -292,12 +427,16 @@ class ADS111XAnalogConf(OptionsDialogGroupBox,CommunicationHandler):
     def onclose(self):
         self.remove_callbacks()
 
+    def numChannelChanged(self,channels):
+        self.processingOptions.setChannels(channels)
+        self.numAinBox.setValue(channels)
 
     def apply(self):
         self.send_value("adsAnalog","diff",1 if self.diffCb.isChecked() else 0)
         self.send_value("adsAnalog","inputs",self.numAinBox.value())
         self.send_value("adsAnalog","gain",int(self.gainCombobox.currentData()))
         self.send_value("adsAnalog","rate",int(self.samplerateCombobox.currentData()))
+        self.processingOptions.setChannels(self.numAinBox.value())
         
     
     def readValues(self):
@@ -307,4 +446,4 @@ class ADS111XAnalogConf(OptionsDialogGroupBox,CommunicationHandler):
         self.get_value_async("adsAnalog","gain",self.gainCombobox.setCurrentIndex,0,conversion=int)
         self.get_value_async("adsAnalog","rate",self.samplerateCombobox.setCurrentIndex,0,conversion=int)
         self.get_value_async("adsAnalog","diff",self.diffCb.setChecked,0,conversion=int)
-        self.get_value_async("adsAnalog","inputs",self.numAinBox.setValue,0,conversion=int)
+        self.get_value_async("adsAnalog","inputs",self.numChannelChanged,0,conversion=int)
---
+++ qrangeslider.py
@@ -0,0 +1,265 @@
+# Based on https://github.com/introkun/qt-range-slider
+from dataclasses import dataclass
+from PyQt6.QtCore import Qt, QRect, QSize, pyqtSignal
+from PyQt6.QtWidgets import (QWidget, QSizePolicy)
+from PyQt6.QtGui import QPainter, QBrush, QColor, QPalette
+
+
+def _left_thumb_adjuster(value, min_value):
+    value = max(value, min_value)
+
+def _right_thumb_adjuster(value, max_value):
+    value = min(value, max_value)
+
+@dataclass
+class Thumb:
+    """Thumb class which holds information about a thumb.
+    """
+    value: int
+    rect: QRect
+    pressed: bool
+
+class QtRangeSlider(QWidget):
+    """
+        QtRangeSlider is a class which implements a slider with 2 thumbs.
+        Methods
+            * __init__ (self, QWidget parent, left_value, right_value, left_thumb_value=0, right_thumb_value=None)
+            * set_left_thumb_value (self, int value):
+            * set_right_thumb_value (self, int value):
+            * (int) get_left_thumb_value (self):
+            * (int) get_right_thumb_value (self):
+        Signals
+            * left_thumb_value_changed (int)
+            * right_thumb_value_changed (int)
+    """
+    HEIGHT = 30
+    WIDTH = 120
+    THUMB_WIDTH = 5
+    THUMB_HEIGHT = 16
+    TRACK_HEIGHT = 3
+    FILL_HEIGHT = 10
+    TRACK_COLOR = QColor(0xc7, 0xc7, 0xc7,127)
+    TRACK_FILL_COLOR = QColor(0x01, 0x81, 0xff,127)
+    TRACK_VAL_COLOR = QColor(0x10, 0xff, 0x1f)
+    TRACK_PADDING = THUMB_WIDTH // 2 + 2
+    TICK_PADDING = 5
+
+    left_thumb_value_changed = pyqtSignal('unsigned long long')
+    right_thumb_value_changed = pyqtSignal('unsigned long long')
+
+    def __init__(self, parent, right_value, left_thumb_value=0, right_thumb_value=None):
+        super().__init__(parent)
+
+        self.setSizePolicy(
+            QSizePolicy.Policy.Expanding,
+            QSizePolicy.Policy.Fixed
+        )
+        self.setMinimumWidth(self.WIDTH)
+        self.setMinimumHeight(self.HEIGHT)
+
+        #self._left_value = left_value
+        self._right_value = right_value
+        #self._value = 0
+
+        self._left_thumb = Thumb(left_thumb_value, None, False)
+        _right_thumb_value = right_thumb_value if right_thumb_value is not None \
+            else self._right_value
+        if _right_thumb_value < left_thumb_value + 1:
+            raise ValueError("Right thumb value is less or equal left thumb value.")
+        self._right_thumb = Thumb(_right_thumb_value, None, False)
+
+        self._canvas_width = None
+        self._canvas_height = None
+
+        self._ticks_count = 0
+
+        parent_palette = parent.palette()
+        self._background_color = parent_palette.color(QPalette.ColorRole.Window)
+        self._base_color = parent_palette.color(QPalette.ColorRole.Base)
+        self._button_color = parent_palette.color(QPalette.ColorRole.Button)
+        self._border_color = parent_palette.color(QPalette.ColorRole.Mid)
+
+
+    # def setValue(self,val : int):
+    #     self._value = val
+
+    def paintEvent(self, unused_e):
+        del unused_e
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        #self.__draw_value(self._canvas_width, self._canvas_height, painter)
+        self.__draw_track(self._canvas_width, self._canvas_height, painter)
+        self.__draw_track_fill(self._canvas_width, self._canvas_height, painter)
+        self.__draw_ticks(self._canvas_width, self._canvas_height, painter, self._ticks_count)
+        self.__draw_left_thumb(self._canvas_width, self._canvas_height, painter)
+        self.__draw_right_thumb(self._canvas_width, self._canvas_height, painter)
+
+        painter.end()
+
+    def __get_track_y_position(self):
+        return self._canvas_height // 2 - self.TRACK_HEIGHT // 2
+
+    def __draw_value(self, canvas_width, canvas_height, painter):
+        del canvas_height
+        brush = QBrush()
+        brush.setColor(self.TRACK_VAL_COLOR)
+        brush.setStyle(Qt.BrushStyle.SolidPattern)
+
+        available_width = canvas_width - 2 * self.TRACK_PADDING
+        x1 = self.TRACK_PADDING#self._left_thumb.value / self._right_value * available_width + self.TRACK_PADDING
+        x2 = self._value / self._right_value * available_width + self.TRACK_PADDING
+        rect = QRect(round(x1), self._canvas_height // 2 - self.FILL_HEIGHT // 2, \
+            round(x2) - round(x1), self.FILL_HEIGHT)
+        painter.fillRect(rect, brush)
+
+    def __draw_track(self, canvas_width, canvas_height, painter):
+        del canvas_height
+        brush = QBrush()
+        brush.setColor(self.TRACK_COLOR)
+        brush.setStyle(Qt.BrushStyle.SolidPattern)
+
+        rect = QRect(self.TRACK_PADDING, self.__get_track_y_position(), \
+            canvas_width - 2 * self.TRACK_PADDING, self.TRACK_HEIGHT)
+        painter.fillRect(rect, brush)
+
+    def __draw_track_fill(self, canvas_width, canvas_height, painter):
+        del canvas_height
+        brush = QBrush()
+        brush.setColor(self.TRACK_FILL_COLOR)
+        brush.setStyle(Qt.BrushStyle.SolidPattern)
+
+        available_width = canvas_width - 2 * self.TRACK_PADDING
+        x1 = self._left_thumb.value / self._right_value * available_width + self.TRACK_PADDING
+        x2 = self._right_thumb.value / self._right_value * available_width + self.TRACK_PADDING
+        rect = QRect(round(x1), self.__get_track_y_position(), \
+            round(x2) - round(x1), self.TRACK_HEIGHT)
+        painter.fillRect(rect, brush)
+
+    # pylint: disable=no-self-use
+    def __set_painter_pen_color(self, painter, pen_color):
+        pen = painter.pen()
+        pen.setColor(pen_color)
+        painter.setPen(pen)
+
+    def __draw_thumb(self, x, y, painter):
+        brush = QBrush()
+        brush.setColor(self._base_color)
+        brush.setStyle(Qt.BrushStyle.SolidPattern)
+
+        self.__set_painter_pen_color(painter, self._border_color)
+
+        painter.setBrush(brush)
+
+        thumb_rect = QRect(round(x) - self.THUMB_WIDTH // 2 + self.TRACK_PADDING, \
+            y + self.TRACK_HEIGHT // 2 - self.THUMB_HEIGHT // 2, self.THUMB_WIDTH, self.THUMB_HEIGHT)
+        painter.drawRect(thumb_rect)
+        return thumb_rect
+
+    def __draw_right_thumb(self, canvas_width, canvas_height, painter):
+        del canvas_height
+        available_width = canvas_width - 2 * self.TRACK_PADDING
+        x = self._right_thumb.value / self._right_value * available_width
+        y = self.__get_track_y_position()
+        self._right_thumb.rect = self.__draw_thumb(x, y, painter)
+
+    def __draw_left_thumb(self, canvas_width, canvas_height, painter):
+        del canvas_height
+        available_width = canvas_width - 2 * self.TRACK_PADDING
+        x = round(self._left_thumb.value / self._right_value * available_width)
+        y = self.__get_track_y_position()
+        self._left_thumb.rect = self.__draw_thumb(x, y, painter)
+
+    def set_left_thumb_value(self, value):
+        if value < 0 or value > self._right_thumb.value - 1:
+            return
+        if value == self._left_thumb.value:
+            # nothing to update
+            return
+        self._left_thumb.value = value
+
+        self.left_thumb_value_changed.emit(value)
+        self.repaint()
+
+    def set_right_thumb_value(self, value):
+        if value > self._right_value or value < self._left_thumb.value + 1:
+            return
+        if value == self._right_thumb.value:
+            # nothing to update
+            return
+        self._right_thumb.value = value
+
+        self.right_thumb_value_changed.emit(value)
+        self.repaint()
+
+    # override Qt event
+    def mousePressEvent(self, event):
+
+        if self._left_thumb.rect.contains(event.pos().x(), event.pos().y()):
+            self._left_thumb.pressed = True
+        if self._right_thumb.rect.contains(event.pos().x(), event.pos().y()):
+            self._right_thumb.pressed = True
+        super().mousePressEvent(event)
+
+    # override Qt event
+    def mouseReleaseEvent(self, event):
+
+        self._left_thumb.pressed = False
+        self._right_thumb.pressed = False
+        super().mouseReleaseEvent(event)
+
+    # pylint: disable=no-self-use
+    def __get_thumb_value(self, x, canvas_width, right_value):
+
+        return round(x / canvas_width * right_value)
+
+    # override Qt event
+    def mouseMoveEvent(self, event):
+
+        thumb = self._left_thumb if self._left_thumb.pressed else self._right_thumb
+
+        if thumb.pressed:
+            if thumb == self._left_thumb:
+                value_setter = self.set_left_thumb_value
+                value_adjuster = lambda val: _left_thumb_adjuster(val, 0)
+            else:
+                value_setter = self.set_right_thumb_value
+                value_adjuster = lambda val: _right_thumb_adjuster(val, self._right_value)
+
+            new_val = self.__get_thumb_value(event.pos().x(), self._canvas_width, self._right_value)
+            value_adjuster(new_val)
+            value_changed = new_val != thumb.value
+            if value_changed:
+                value_setter(new_val)
+
+        super().mouseMoveEvent(event)
+
+    def get_left_thumb_value(self):
+        return self._left_thumb.value
+
+    def get_right_thumb_value(self):
+        return self._right_thumb.value
+
+    def set_ticks_count(self, count):
+        if count < 0:
+            raise ValueError("Invalid ticks count.")
+        self._ticks_count = count
+
+    def __draw_ticks(self, canvas_width, canvas_height, painter, ticks_count):
+        del canvas_height
+        if not self._ticks_count:
+            return
+
+        self.__set_painter_pen_color(painter, self._border_color)
+
+        tick_step = (canvas_width - 2 * self.TRACK_PADDING) // ticks_count
+        y1 = self.__get_track_y_position() - self.TICK_PADDING
+        y2 = y1 - self.THUMB_HEIGHT // 2
+        for x in range(0, ticks_count + 1):
+            x = x * tick_step + self.TRACK_PADDING
+            painter.drawLine(x, y1, x, y2)
+
+    def resizeEvent(self, event):
+        del event
+        self._canvas_width = self.width()
+        self._canvas_height = self.height()
