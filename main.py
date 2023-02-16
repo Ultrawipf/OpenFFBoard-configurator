f9ed86d36f6de09a5ae69ddf8bb1f4763def100f
Yannick Richter <git@y-richter.de>
2023-02-16 16:28:46 +0100

    Fixed encoder tune UI spinbox

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ import updater
 import simplemotion_ui
 
 # This GUIs version
-VERSION = "1.12.1"
+VERSION = "1.12.2"
 
 # Minimal supported firmware version.
 # Major version of firmware must match firmware. Minor versions must be higher or equal
